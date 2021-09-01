9d95e4f4c81e70370d7793d7d9e8653ceba62bb7
Andreyskey <be0rn99@icloud.com>
2021-09-02 02:24:14 +0300

    Homework Lesson-4

---
+++ VK-StoryBoard/VK-StoryBoard/CustomControl.swift
@@ -0,0 +1,15 @@
+//
+//  CustomControl.swift
+//  VK-StoryBoard
+//
+//  Created by Андрей Волков on 01.09.2021.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+}
